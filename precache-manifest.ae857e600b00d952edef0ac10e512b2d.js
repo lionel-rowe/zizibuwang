e136cb536b1ebf4e64445b2103070e919a9690c9
Lionel Rowe <lionel.rowe@gmail.com>
2020-02-27 00:37:09 +0000

    Updates

---
+++ precache-manifest.ae857e600b00d952edef0ac10e512b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06acdd3898b2a0f876942ed254e0145",
+    "url": "/zizibuwang/index.html"
+  },
+  {
+    "revision": "2ad56b87755c45c4d3d2",
+    "url": "/zizibuwang/static/css/main.eb6c3837.chunk.css"
+  },
+  {
+    "revision": "129b4a01fe7739e5e9c4",
+    "url": "/zizibuwang/static/js/2.5e943e47.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/zizibuwang/static/js/2.5e943e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad56b87755c45c4d3d2",
+    "url": "/zizibuwang/static/js/main.e02d3736.chunk.js"
+  },
+  {
+    "revision": "e3c699305932e917d128",
+    "url": "/zizibuwang/static/js/runtime-main.3fcb1d39.js"
+  }
+]);
