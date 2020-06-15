9eb74ca2f15a7b19ae20ec99ec56d369db4bc2af
Lionel Rowe <lionel.rowe@gmail.com>
2020-06-15 20:28:16 +0100

    Updates

---
+++ precache-manifest.0eebc943430152087f22525d9f1e39cd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee89d840e7a30b0ddae9312766eaf312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45ac0a76398b27f8433b",
+    "url": "/static/css/main.fb014dec.chunk.css"
+  },
+  {
+    "revision": "1ed1bfbe2c339c129932",
+    "url": "/static/js/2.b3038c51.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.b3038c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ac0a76398b27f8433b",
+    "url": "/static/js/main.f20c4b55.chunk.js"
+  },
+  {
+    "revision": "0feb3b77f3c0acd809d3",
+    "url": "/static/js/runtime-main.fdb1e4d5.js"
+  },
+  {
+    "revision": "c382153c00d4ec790424d0696fb48fed",
+    "url": "/static/media/ZhiMangXing-Regular-subset.c382153c.ttf"
+  },
+  {
+    "revision": "02fc05692ad84ee7d95d35bc1eb03fb4",
+    "url": "/static/media/cc-cedict.02fc0569.tsv"
+  },
+  {
+    "revision": "3cb44addd7876ab9f2138bc43f5abb4c",
+    "url": "/static/media/instructions.3cb44add.md"
+  },
+  {
+    "revision": "42676c8bc15dc494e5dbb130acc84d48",
+    "url": "/static/media/pinyin-syllables.42676c8b.tsv"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zizibuwang/precache-manifest.19fc4f17df626145ebe2b25d9b88e013.js"
+  "/precache-manifest.0eebc943430152087f22525d9f1e39cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zizibuwang/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
